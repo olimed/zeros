baf0426762a0758fba95b6b07411b8873dea27a9
olimed <olmedvedskaya@gmail.com>
2018-02-27 14:09:14 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+ 
+  let numberModFiveWithoutRest = 5;
+  let zerosCount = 0;
+
+  while (number >= 5){
+    number = Math.floor(number / 5);
+    zerosCount = zerosCount + number;
+  }
+
+  return zerosCount
+  
 }
